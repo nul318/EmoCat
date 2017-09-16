f2660d7ad7bf88e79996e7f68f2945f8eaba262b
Wonong <ciwnyg0815@gmail.com>
2017-09-16 14:27:17 +0900

    ajax add

--- EmoCat_web/web/dist/js/ajax.js
+++ EmoCat_web/web/dist/js/ajax.js
@@ -1,5 +1,6 @@
 $(document).ready(function($){
-  var dest = "http://45.76.99.126";
+  // var dest = "http://45.76.99.126";
+  var dest = "http://localhost:3000";
   $.get(dest + '/info/device/one_device2/1', function(res){
     console.log(res);
   });
